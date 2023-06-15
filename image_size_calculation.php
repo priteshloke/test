0e9111ba961aba145e1ddac5e5190936331b174b
Pritesh Loke <pritesh.loke@timesinternet.in>
2023-06-15 15:56:51 +0530

    updated

--- image_size_calculation.php
+++ image_size_calculation.php
@@ -3,6 +3,7 @@ class MatchImageSize
 {
     private $__firstImageSizes = [];
     private $__secondImageSizes = [];
+
     public function __construct($firstImageSizes, $secondImageSizes)
     {
         $this->__firstImageSizes = $firstImageSizes;
@@ -18,9 +19,17 @@ class MatchImageSize
 
         $imageratio = $this->__secondImageSizes['width'] / $this->__secondImageSizes['height'];
 
+        print("ratio {$imageratio} \n");
+
         if ($this->__secondImageSizes['width'] > $this->__firstImageSizes['width']) {
             $newWidth = $this->__firstImageSizes['width'];
             $newHeight = round($newWidth / $imageratio);
+        } else if ($this->__secondImageSizes['height'] > $this->__firstImageSizes['height']) {  
+            $newWidth = $this->__secondImageSizes['width'];
+            $newHeight = ($this->__firstImageSizes['height'] * $this->__secondImageSizes['width']) / $this->__firstImageSizes['width'];
+
+            // $newimageratio = $newWidth / $newHeight;
+            // print("new ratio {$newimageratio} \n");
         } else {
             return $this->__secondImageSizes;
         }
@@ -50,14 +59,17 @@ class MatchImageSize
     }
 }
 
-$firstIimageSizes[] = ['width' => 180, 'height' => 250];
-$secondImageSizes[] = ['width' => 360, 'height' => 200];
+// $firstIimageSizes[] = ['width' => 180, 'height' => 250];
+// $secondImageSizes[] = ['width' => 360, 'height' => 200];
 
 // $firstIimageSizes[] = ['width' => 250, 'height' => 500];
 // $secondImageSizes[] = ['width' => 500, 'height' => 90];
 
 // $firstIimageSizes[] = ['width' => 1000, 'height' => 500];
 // $secondImageSizes[] = ['width' => 500, 'height' => 90];
+
+$firstIimageSizes[] = ['width' => 250, 'height' => 250];
+$secondImageSizes[] = ['width' => 150, 'height' => 260];
 
 foreach($firstIimageSizes as $key => $values) {
     $matchImage = new MatchImageSize($values, $secondImageSizes[$key]);
